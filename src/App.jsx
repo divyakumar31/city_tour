b607775e158ec3e46e2df6585ab2d2de2dccee59
Dev Patel <devapatel0603@gmail.com>
2024-07-21 14:01:12 +0530

    Footer and Routing Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Home from "./routes/Home";
+import Footer from "./components/footer/Footer";
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components";
 
 const App = () => {
-  return <Home />;
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={() => <h1>About</h1>} />
+                <Route path="/contact" component={() => <h1>Contact</h1>} />
+                <Route path="/map" component={() => <h1>Map</h1>} />
+            </Routes>
+            <Footer />
+        </>
+    );
 };
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
 const Navbar = () => {
-  return (
-    <nav class="flex">
-      <ul class="flex">
-        <li>
-          <a href="#">HOME</a>
-        </li>
-        <li>
-          <a href="#history">HISTORY</a>
-        </li>
-        <li>
-          <a href="#">ABOUT</a>
-        </li>
-        <li>
-          <a href="#popular">POPULAR</a>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="flex">
+            <ul className="flex">
+                <li>
+                    <a href="#">HOME</a>
+                </li>
+                <li>
+                    <a href="#history">HISTORY</a>
+                </li>
+                <li>
+                    <a href="#">ABOUT</a>
+                </li>
+                <li>
+                    <a href="#popular">POPULAR</a>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
