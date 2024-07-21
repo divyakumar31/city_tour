0a48665ecf0f0d0c201e577de0d3a46364b49a93
divyakumar patel <d.b.patel691@gmail.com>
2024-07-21 14:06:25 +0530

    navbar completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import "./navbar.css";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
-    <nav class="flex">
-      <ul class="flex">
+    <nav className="flex navbar">
+      <ul className={`flex ${menu ? "d-block" : ""}`}>
         <li>
           <a href="#">HOME</a>
-        </li>
-        <li>
-          <a href="#history">HISTORY</a>
         </li>
         <li>
           <a href="#">ABOUT</a>
         </li>
         <li>
-          <a href="#popular">POPULAR</a>
+          <a href="#">PLACES TO VISIT</a>
+        </li>
+        <li>
+          <a href="#">EXPERIENCES</a>
+        </li>
+        <li>
+          <a href="#">HERITAGE WALK</a>
+        </li>
+        <li>
+          <a href="#">MAP</a>
+        </li>
+        <li>
+          <a href="#">CONSERVATION</a>
+        </li>
+        <li>
+          <a href="#">CONTACT</a>
         </li>
       </ul>
+      <div
+        className={`menu ${menu ? "close-btn" : ""}`}
+        onClick={() => (setMenu(!menu), console.log("menu clicked", menu))}
+      >
+        <div className={`bar1 menu-bar`}></div>
+        <div className={`bar2 menu-bar`}></div>
+        <div className={`bar3 menu-bar`}></div>
+      </div>
     </nav>
   );
 };
